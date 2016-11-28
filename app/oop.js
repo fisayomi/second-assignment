43fdbf6a4e6b0fa2cbdc8ef93f397739ceddb66e
Fisayomi <fisayomi@users.noreply.github.com>
2016-11-28 16:13:33 +0100

    Made some corrections

--- app/oop.js
+++ app/oop.js
@@ -1,4 +1,5 @@
 
+
 var Car = function (name, model, type){
 	this.type = type;
 	this.model = model;
@@ -11,6 +12,7 @@ var Car = function (name, model, type){
 	if(this.type == 'trailer'){
 			this.numOfWheels = 8;
 			this.isSaloon = false;
+			this.speed = '0 km/h';
 		}
 	else {
 		this.numOfWheels = 4;
@@ -18,28 +20,71 @@ var Car = function (name, model, type){
 	}
 	
 
-	if(this.name == undefined){
+	if(this.name === undefined){
 		this.name = 'General';
 		this.model = 'GM';
 	}
-	this.speed = '0 km/h';
-
-}
-
-
-
-Car.prototype.drive = function (){
-
-	if (this.name == 'Porshe') {
-		this.speed = '250 km/h';
-	}
 	
-	if (this.type == 'trailer') {
-		 this.speed = '77 km/h';
-	}
-	return this;
 }
 
+
+
+Car.prototype.drive = function (num){
+
+	if(num == 5){
+			this.speed = '250 km/h';
+		}
+		else if (num == 7){
+			this.speed = '77 km/h';
+		}
+		return this;
+	}
 
 
 module.exports = {Car:Car}
+
+//SECOND METHOD
+
+/*class Car {
+	constructor(name,model,type){
+		this.name = name;
+		this.model = model;
+		this.type = type;
+
+		if(this.name == 'Porshe' || this.name == 'Koenigsegg'){
+			this.numOfDoors = 2;
+		}
+		else this.numOfDoors = 4;
+
+		if(this.type == 'trailer'){
+				this.numOfWheels = 8;
+				this.isSaloon = false;
+			}
+		else {
+			this.numOfWheels = 4;
+			this.isSaloon = true;
+		}
+		
+
+		if(this.name == undefined){
+			this.name = 'General';
+			this.model = 'GM';
+		}
+		this.speed = '0 km/h';
+
+	}
+
+	
+
+	drive(num){
+		if(num == 5){
+			this.speed = '250 km/h';
+		}
+		else if (num == 7){
+			this.speed = '77 km/h';
+		}
+		return this;
+	}
+
+
+}*/
